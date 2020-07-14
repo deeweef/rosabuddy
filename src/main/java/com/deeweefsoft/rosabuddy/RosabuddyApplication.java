3b878f8927745471e9fae59ddf0e37d09ec1a134
Dag Findal-Fossmo <dag.findalfossmo@gmail.com>
2020-07-14 15:36:22 +0200

    Initial commit

---
+++ src/main/java/com/deeweefsoft/rosabuddy/RosabuddyApplication.java
@@ -0,0 +1,13 @@
+package com.deeweefsoft.rosabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosabuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosabuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deeweefsoft/rosabuddy/RosabuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeweefsoft.rosabuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosabuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
